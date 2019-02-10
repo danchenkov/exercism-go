6821914365156c61720141d985ab22454d52427f
Alexei Danchenkov <alexeid@scientologymedia.net>
2019-02-10 02:17:59 -0800

    attempt to improve

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -3,11 +3,17 @@ package robotname
 import (
 	"fmt"
 	"math/rand"
-	"strings"
 	"time"
 )
 
 const testVersion = 1
+
+const letterBytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
 
 var names map[string]struct{}
 
@@ -19,16 +25,27 @@ func init() {
 	names = make(map[string]struct{})
 }
 
+func randStringBytesMaskImprSrc(n int) string {
+	var src = rand.NewSource(time.Now().UnixNano())
+	b := make([]byte, n)
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return string(b)
+}
+
 func generateName() string {
-	rand.Seed(time.Now().UnixNano())
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	var b strings.Builder
 	var name string
 	for ok := true; ok; _, ok = names[name] {
-		for i := 0; i < 2; i++ {
-			b.WriteRune(chars[rand.Intn(len(chars))])
-		}
-		name = b.String() + fmt.Sprintf("%03d", rand.Intn(1000))
+		name = randStringBytesMaskImprSrc(2) + fmt.Sprintf("%03d", rand.Intn(1000))
 	}
 	names[name] = struct{}{}
 	return name
