1c11481607e6eb04c4c8f6164be5a54552250a8a
Alexei Danchenkov <alexeid@scientologymedia.net>
2019-10-24 23:38:06 -0700

    RNA transcription

---
+++ rna-transcription/rna_transcription.go
@@ -0,0 +1,18 @@
+package strand
+
+func ToRNA(dna string) string {
+	rna := make([]byte, len(dna))
+	for i := 0; i < len(dna); i++ {
+		switch dna[i] {
+		case 'A':
+			rna[i] = 'U'
+		case 'C':
+			rna[i] = 'G'
+		case 'G':
+			rna[i] = 'C'
+		case 'T':
+			rna[i] = 'A'
+		}
+	}
+	return string(rna)
+}
