19ca4dd346488efeee211952c6e142253ecf2b9e
Alexei Danchenkov <alexeid@scientologymedia.net>
2019-10-28 18:16:43 -0700

    minor improvements

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -2,7 +2,6 @@ package tournament
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
 	"sort"
@@ -17,56 +16,21 @@ type TeamResult struct {
 	losses  int
 }
 
-func (t *TeamResult) points() int {
+func (t TeamResult) points() int {
 	return t.wins*3 + t.draws
 }
 
 // Ranking is used for sorting
 type Ranking struct {
 	team   string
-	result *TeamResult
-}
-
-func win(team1, team2 string, results map[string]*TeamResult) map[string]*TeamResult {
-	if r, ok := results[team1]; ok {
-		r.matches++
-		r.wins++
-		results[team1] = r
-	} else {
-		results[team1] = &TeamResult{matches: 1, wins: 1}
-	}
-	if r, ok := results[team2]; ok {
-		r.matches++
-		r.losses++
-		results[team2] = r
-	} else {
-		results[team2] = &TeamResult{matches: 1, losses: 1}
-	}
-	return results
-}
-
-func draw(team1, team2 string, results map[string]*TeamResult) map[string]*TeamResult {
-	if r, ok := results[team1]; ok {
-		r.matches++
-		r.draws++
-		results[team1] = r
-	} else {
-		results[team1] = &TeamResult{matches: 1, draws: 1}
-	}
-	if r, ok := results[team2]; ok {
-		r.matches++
-		r.draws++
-		results[team2] = r
-	} else {
-		results[team2] = &TeamResult{matches: 1, draws: 1}
-	}
-	return results
+	result TeamResult
 }
 
 // Tally reads slice of strings with raw results; writes current tournament standing
 func Tally(r io.Reader, w io.Writer) error {
-	results := make(map[string]*TeamResult)
+	results := map[string]TeamResult{}
 	var rankings []Ranking
+	var a, b TeamResult
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
@@ -75,39 +39,50 @@ func Tally(r io.Reader, w io.Writer) error {
 			continue
 		}
 
-		if strings.HasPrefix(line, "#") || strings.HasPrefix(line, "//") {
+		if strings.HasPrefix(line, "#") {
 			continue
 		}
 
 		if strings.Count(scanner.Text(), ";") != 2 {
-			return errors.New("Invalid input: " + scanner.Text())
+			return fmt.Errorf("Invalid input: %q", scanner.Text())
 		}
 
 		result := strings.Split(scanner.Text(), ";")
-		if len(result[0]) == 0 || strings.HasPrefix(result[0], "#") || strings.HasPrefix(result[0], "//") {
-			return errors.New("Invalid team name: " + result[0])
+		if len(result[0]) == 0 {
+			return fmt.Errorf("Invalid team name: %q", result[0])
 		}
-		if len(result[1]) == 0 || strings.HasPrefix(result[1], "#") || strings.HasPrefix(result[1], "//") {
-			return errors.New("Invalid team name: " + result[1])
+		if len(result[1]) == 0 {
+			return fmt.Errorf("Invalid team name: %q", result[1])
 		}
 
+		a = results[result[0]]
+		b = results[result[1]]
 		switch result[2] {
 		case "win":
-			results = win(result[0], result[1], results)
+			a.matches++
+			a.wins++
+			b.matches++
+			b.losses++
 		case "loss":
-			results = win(result[1], result[0], results)
+			b.matches++
+			b.wins++
+			a.matches++
+			a.losses++
 		case "draw":
-			results = draw(result[0], result[1], results)
+			a.matches++
+			a.draws++
+			b.matches++
+			b.draws++
 		default:
-			return errors.New("Invalid result: " + result[2])
+			return fmt.Errorf("Bad match result: %q", result[2])
 		}
+		results[result[0]] = a
+		results[result[1]] = b
+
 	}
 
 	for k, v := range results {
-		r := new(Ranking)
-		r.team = k
-		r.result = v
-		rankings = append(rankings, *r)
+		rankings = append(rankings, Ranking{team: k, result: v})
 	}
 	sort.Slice(rankings, func(i, j int) bool {
 		if rankings[i].result.points() == rankings[j].result.points() {
